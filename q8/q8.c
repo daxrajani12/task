33ca6f8c6aff8fa4266e047262f420547d794d5a
daxrajani12 <daxr@glidemtech.com>
2023-01-27 12:24:47 +0530

    first commit

--- q8/q8.c
+++ q8/q8.c
@@ -2,13 +2,18 @@
 // Make a Makefile to compile these c files.
 
 #include <stdio.h>
+#include<string.h>
 #include "string_compare.c"
 
 int string_compare(char* str1, char* str2);
 
 int main() {
-    char str1[] = "Dax";
-    char str2[] = "Dax rajani";
+    char str1[1024];
+    char str2[1024];
+    printf("Enter string 1: ");
+    fgets(str1,1024,stdin);
+    printf("Enter string 2: ");
+    fgets(str2,1024,stdin);
     int result = string_compare(str1, str2);
     if (result == 0) {
         printf("The strings are equal\n");
